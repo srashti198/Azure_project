7e22d111d860d38821373037768246d5526146c7
sunil gori <59092279+sunil2206@users.noreply.github.com>
2022-09-21 19:39:07 +0530

    Update Startup.cs

--- Azure_CICD_WebApi/Startup.cs
+++ Azure_CICD_WebApi/Startup.cs
@@ -14,6 +14,7 @@ using System.Threading.Tasks;
 
 namespace Azure_CICD_WebApi
 {
+     [ExcludeFromCodeCoverage]
     public class Startup
     {
         public Startup(IConfiguration configuration)
