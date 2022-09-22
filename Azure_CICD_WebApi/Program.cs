12b70f4d554523ffb688f496aaa69e9d15682801
sunil gori <59092279+sunil2206@users.noreply.github.com>
2022-09-22 09:22:13 +0530

    Update Program.cs

--- Azure_CICD_WebApi/Program.cs
+++ Azure_CICD_WebApi/Program.cs
@@ -9,7 +9,7 @@ using System.Threading.Tasks;
 
 namespace Azure_CICD_WebApi
 {
-     [ExcludeFromCodeCoverage]
+     
     public class Program
     {
         public static void Main(string[] args)
